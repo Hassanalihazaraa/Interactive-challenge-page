4eacb91dbb39d76afd1e78a3afea1c7fc11ba617
Becode <stevekeersma@gmail.com>
2020-05-29 15:26:43 +0200

    working js

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-var slideIndex =1;
+var slideIndex = 1;
 var i;
 var slides = document.getElementsByClassName("slide-img");
-slides[slideIndex - 1].style.display = "block";
+slides[slideIndex].style.display = "block";
 
 function display() {
     if (slideIndex > slides.length) {
-        slideIndex =1;
-    }
-    if (slideIndex < 0) {
-        slideIndex = slides.length;
+        slideIndex = 1;
     }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slides[slideIndex - 1].style.display = "block";
+    if (slideIndex == 3) {
+        slideIndex = 0;
+    }
+    if (slideIndex < 0) {
+        slideIndex = 2;
+    }
+    console.log(slideIndex);
+    if (slideIndex >= 0 && slideIndex < 3) {
+        slides[slideIndex].style.display = "block";
+    }
 }
 
 document.getElementById("next").onclick = function () {
@@ -26,4 +32,61 @@ document.getElementById("prev").onclick = function () {
     display();
 
 };
-console.log(slideIndex);
+
+
+//tabs
+
+function showTab(x) {
+    for (i = 0; i < contents.length; i++) {
+        contents[i].style.display = "none";
+    }
+    if (x !== null) {
+        contents[x].style.display = "block";
+    }
+}
+
+contents = document.getElementsByClassName("tabContent");
+showTab(null);
+
+document.getElementById("summary-btn").onclick = function () {
+    showTab(0)
+};
+document.getElementById("practical-btn").onclick = function () {
+
+    showTab(1)
+
+};
+document.getElementById("contact-btn").onclick = function () {
+    showTab(2)
+
+};
+document.getElementById("button").disabled = true;
+document.getElementById("fullname").onkeyup = function () {
+
+    if (document.getElementById("fullname").value == "" || document.getElementById("email").value == "" ||
+        document.getElementById("textarea").value == "") {
+        document.getElementById("button").disabled = true;
+    } else {
+        document.getElementById("button").disabled = false;
+    }
+};
+document.getElementById("email").onkeyup = function () {
+
+
+    if (document.getElementById("fullname").value == "" || document.getElementById("email").value == "" ||
+        document.getElementById("textarea").value == "") {
+        document.getElementById("button").disabled = true;
+    } else {
+        document.getElementById("button").disabled = false;
+    }
+};
+document.getElementById("textarea").onkeyup = function () {
+
+
+    if (document.getElementById("fullname").value == "" || document.getElementById("email").value == "" ||
+        document.getElementById("textarea").value == "") {
+        document.getElementById("button").disabled = true;
+    } else {
+        document.getElementById("button").disabled = false;
+    }
+};
